709dbd13acd86c6d478db542e1b42819c0ddc8ae
rahulvijay81 <rahulvjay8156@gmail.com>
2023-03-19 12:33:01 +0530

    firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,13 +7,13 @@ import 'firebase/firestore'
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKR8F65sc2aYpO56Kj_Bz8J4-FvWOIvoY",
-  authDomain: "olxclone-1c2a9.firebaseapp.com",
-  projectId: "olxclone-1c2a9",
-  storageBucket: "olxclone-1c2a9.appspot.com",
-  messagingSenderId: "1052520497808",
-  appId: "1:1052520497808:web:39d2b57572762a63ca4e15",
-  measurementId: "G-3ZP3K8VCY0"
+  apiKey: "AIzaSyD2m6rTxBrQ7F8_3SFwCilUWphR8gDAy9Y",
+  authDomain: "olx-clone-7e9ce.firebaseapp.com",
+  projectId: "olx-clone-7e9ce",
+  storageBucket: "olx-clone-7e9ce.appspot.com",
+  messagingSenderId: "685224333046",
+  appId: "1:685224333046:web:06b1469ae4358089bbe6d7",
+  measurementId: "G-H16PB9QHNH"
 };
 
 export default firebase.initializeApp(firebaseConfig);
